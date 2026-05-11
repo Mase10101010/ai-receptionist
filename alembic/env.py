02a0e7b2ea31f36f57cfcad4e5e52989e45faf3b
Bone Cold <alessandromasiero000@gmail.com>
2026-05-11 18:06:45 +0800

    fix alembic sync database url

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,7 @@ from app.db.base import Base
 from app.models import *  # noqa: F401, F403 — needed for autogenerate
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.SYNC_DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.sync_database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
